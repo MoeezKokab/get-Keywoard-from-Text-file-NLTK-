db0cb181cae0c4b0fccf951d3c3cd8eb8c0c3bfb
MoeezKokab <45976097+MoeezKokab@users.noreply.github.com>
2021-03-23 19:43:06 +0500

    Add files via upload

--- keyword.py
+++ keyword.py
@@ -6,28 +6,52 @@ Created on Mon Mar 22 20:07:32 2021
 """
 
 from nltk.tokenize import RegexpTokenizer
-from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+#read text from text file
 text= open("subtitle.txt", "r")
-   
-    #print(text.read())
+
+# covert into str
 data = text.read()
-   # print(data)
-wordToken = word_tokenize(data)
-print(wordToken)
-t= 1
+
 #Tokenizer
 tokenizer = RegexpTokenizer(r'\w+')
 tok = tokenizer.tokenize(data)
-print(tok)
+#print(tok)
 
 #remove stop word
 stop_word = set(stopwords.words("english"))
 sw = []
 for word in tok:
     if word not in stop_word:
-print(sw)
+        sw.append(word)
+#print(sw)
 
+# stemmer
+stemmer = []
+from nltk.stem import PorterStemmer
+ps = PorterStemmer()
+for word in sw:
+        stemmer.append(ps.stem(word))
+print(stemmer)
+        
+#correect spell
+from autocorrect import spell
+cw =[]
+for word in stemmer:
+    cw.append(spell(word))
 
+# frq
+from collections import Counter
+frq = Counter(cw)
+#print(frq)
+ls=[]
+for key in frq.keys():
+    ls.append(key)
+ls.sort()
+print(ls)
+#save keywoard
+text_file = open("keywoards.txt", "w")
+text_file.write(str(ls))
+text_file.close()
 
 text.close()
